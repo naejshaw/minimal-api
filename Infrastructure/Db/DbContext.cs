38fce57668f9f9b213f81806a23f6916f177bc8d
Jean <89807593+naejshaw@users.noreply.github.com>
2024-09-03 19:23:51 -0300

    test

--- Infrastructure/Db/DbContext.cs
+++
@@ -1,5 +0,0 @@
-namespace MinimalApi.Infrastructure.Db;
-
-public class DbContext{
-    
-}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
     // Login logic here
     if(loginDTO.Email == "admin@email.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login realizado com sucesso");
